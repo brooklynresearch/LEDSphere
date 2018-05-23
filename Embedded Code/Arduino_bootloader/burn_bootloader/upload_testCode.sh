0b88f4de874ddbdb997cc486e8565f2907c87f7e
Deqing Sun <iamseer@gmail.com>
2018-05-22 22:19:21 -0400

    update code

---
+++ Embedded Code/Arduino_bootloader/burn_bootloader/upload_testCode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+avrdudePath='/Users/sundeqing/Library/Arduino15/packages/arduino/tools/avrdude/6.3.0-arduino9'
+
+#erase and set fuse, D4 fuse indicates an 1024bytes bootloader 
+$avrdudePath/bin/avrdude -C$avrdudePath/etc/avrdude.conf -v -patmega328p -cstk500v2 -Pusb -e -Ulock:w:0x3F:m -Uefuse:w:0xFD:m -Uhfuse:w:0xD4:m -Ulfuse:w:0xFF:m
+#program and lock bootloader
+$avrdudePath/bin/avrdude -C$avrdudePath/etc/avrdude.conf -v -patmega328p -cstk500v2 -Pusb -Uflash:w:testLED_bootloader.hex:i -Ulock:w:0x0F:m 
+
--- Embedded Code/testLED/testLED.ino
+++ Embedded Code/testLED/testLED.ino
@@ -1,4 +1,3 @@
-#define device_ADDR 1
 
 #include <EEPROM.h>
 
@@ -11,21 +10,6 @@ Simple_NeoPixel pixels = Simple_NeoPixel(LED_PIN, NEO_GRB + NEO_KHZ800);
 void setup()
 {
   unsigned char i;
-  Serial.begin(9600);
-  delay(1000);
-  for (i = 0; i < 3; i++) {
-    if (EEPROM.read(i) != device_ADDR) break;
-  }
-  if (i >= 3) {
-    Serial.println("No need to write");
-  }
-  else {
-    for (i = 0; i < 3; i++) {
-      EEPROM.write(i, device_ADDR);
-    }
-    Serial.println("Write OK");
-  }
-
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
