60b344301370ed23a9eb64938d8f817bf5797bf5
Deqing Sun <iamseer@gmail.com>
2018-09-21 22:27:57 -0400

    change firmware

--- Embedded Code/Arduino_bootloader/upload_eeprom/burn_n_id.sh
+++ Embedded Code/Arduino_bootloader/upload_eeprom/burn_n_id.sh
@@ -5,4 +5,4 @@ serialPort='/dev/cu.usbserial-A96LT7BB'
 python generate_eeprom_content.py $1
 
 #program and lock bootloader
-$avrdudePath/bin/avrdude -C$avrdudePath/etc/avrdude.conf -patmega328p -carduino -P$serialPort -b115200 -D -Uflash:w:firmware20180610.hex:i -Ueeprom:w:ledsphere_id_eeprom.hex:i 
+$avrdudePath/bin/avrdude -C$avrdudePath/etc/avrdude.conf -patmega328p -carduino -P$serialPort -b115200 -D -Uflash:w:sketch_485Board20180831.hex:i -Ueeprom:w:ledsphere_id_eeprom.hex:i 
